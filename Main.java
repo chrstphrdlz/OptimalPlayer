786f42f1e184f5f5f7c3fabcb6bdde1fffdd4994
Chris <chris@ubuntu.(none)>
2013-12-23 20:53:12 -0500

    Now works

--- Main.java
+++ Main.java
@@ -3,122 +3,143 @@ public class Main
 {
 	public static void play(Tile aiPlayer, boolean cliTest)
 	{
-		int scenareo = 1;
+		int scenareo = 0;
 		Scanner input = new Scanner(System.in);	
 		EnemyPlayer ai = new EnemyPlayer(aiPlayer);
-		int x,y,cliInput;
+		int x,y,cliInput,numReads=0;
 		String playAgain;
 		boolean successfulMove;
-		boolean keepLooping = true, impossibleMove;
-
-		while(keepLooping)
+		boolean stillPlaying = true, impossibleMove, playAnotherGame = true;
+		while(input.hasNextInt() && playAnotherGame)
 		{
-			if(!cliTest)
-				System.out.println(ai);
-			
-			if(aiPlayer == Tile.X)
+			//System.out.println("a");
+			while(stillPlaying)
 			{
-				try
-				{
-					ai.makeOptimalMove();
-				}
-				catch(Exception e)
-				{
-					System.out.println("Error in scenareo " + scenareo);
-				}
+				//System.out.println("b");
 				if(!cliTest)
 					System.out.println(ai);
+				
+				if(aiPlayer == Tile.X)
+				{
+					try
+					{
+						ai.makeOptimalMove();
+					}
+					catch(Exception e)
+					{
+						System.out.println("Error in scenareo " + scenareo);
+					}
+					if(!cliTest)
+						System.out.println(ai);
+
+					if(ai.winner()!=Tile.Empty)
+					{
+						stillPlaying = false;
+						break;
+					}
+
+					do
+					{
+						impossibleMove = false;
+
+						if(cliTest)
+						{
+							numReads++;
+							cliInput = input.nextInt();
+							x = cliInput/3;
+							y = cliInput%3;
+						}
+						else
+						{
+							x = input.nextInt();
+							y = input.nextInt();
+						}
+
+						if(!ai.playerMakeMove(x,y))
+						{
+							impossibleMove=true;
+
+							if(!cliTest)
+								System.out.println("Cannot move there!");
+						}
+					}
+					while(impossibleMove);
+
+				}
+				else
+				{
+					do
+					{
+						impossibleMove=false;
+
+						if(cliTest)
+						{
+							numReads++;
+							cliInput = input.nextInt();
+							x = cliInput/3;
+							y = cliInput%3;
+						}
+						else
+						{
+							x = input.nextInt();
+							y = input.nextInt();
+						}
+
+						if(!ai.playerMakeMove(x,y))
+						{
+							impossibleMove=true;
+
+							if(!cliTest)
+								System.out.println("Cannot move there!");
+						}
+					}
+					while(impossibleMove);
+
+					if(!cliTest)
+						System.out.println(ai);
+
+					if(ai.winner()!=Tile.Empty)
+					{
+						stillPlaying = false;
+						break;
+					}
+					try{
+					ai.makeOptimalMove();}
+					catch(Exception e)
+					{
+						System.out.println(scenareo);
+						System.out.println("Moved to " +x +" "+y);
+					}
+				}
 
 				if(ai.winner()!=Tile.Empty)
-				{
-					keepLooping = false;
-					break;
-				}
+					stillPlaying = false;
 
-				do
-				{
-					impossibleMove = false;
 
+				else if(ai.winner() != aiPlayer && ai.winner() != Tile.Tie && ai.winner() != Tile.Empty)
+				{
 					if(cliTest)
-					{
-						cliInput = input.nextInt();
-						x = cliInput/3;
-						y = cliInput%3;
-					}
-					else
-					{
-						x = input.nextInt();
-						y = input.nextInt();
-					}
-
-					if(!ai.playerMakeMove(x,y))
-					{
-						impossibleMove=true;
-
-						if(!cliTest)
-							System.out.println("Cannot move there!");
-					}
-				}
-				while(impossibleMove);
-
-			}
-			else
-			{
-				do
-				{
-					impossibleMove=false;
-
-					if(cliTest)
-					{
-						cliInput = input.nextInt();
-						x = cliInput/3;
-						y = cliInput%3;
-					}
-					else
-					{
-						x = input.nextInt();
-						y = input.nextInt();
-					}
-
-					if(!ai.playerMakeMove(x,y))
-					{
-						impossibleMove=true;
-
-						if(!cliTest)
-							System.out.println("Cannot move there!");
-					}
-				}
-				while(impossibleMove);
-
-				if(!cliTest)
-					System.out.println(ai);
-
-				if(ai.winner()!=Tile.Empty)
-				{
-					keepLooping = false;
-					break;
-				}
-
-				ai.makeOptimalMove();
+						System.out.println("Failed on scenareo " + scenareo + " "+ai.winner());
+				}		
 			}
 
-			if(ai.winner()!=Tile.Empty)
-				keepLooping = false;
+			if(cliTest && numReads < 9)
+				input.nextLine();
 
-			else if(ai.winner() != aiPlayer && ai.winner() != Tile.Tie && ai.winner() != Tile.Empty)
-			{
-				if(cliTest)
-					System.out.println("Failed on scenareo " + scenareo + " "+ai.winner());
-			}
+			numReads = 0;
+
+			ai.reset();
+
+			stillPlaying = true;
 
 			scenareo++;
-
-
-		
 		}
 
 		if(!cliTest)
 			System.out.println(ai);
+
+		if(cliTest)
+			System.out.println(scenareo + " scenareos ran");
 	}
 
 
