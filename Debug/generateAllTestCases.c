905c435dee64aaeed5ed2c10fef2199c4f328e70
Chris <chris@ubuntu.(none)>
2013-12-25 11:15:44 -0500

    Added file printing so no longer need to pipe output to file

--- Debug/generateAllTestCases.c
+++ Debug/generateAllTestCases.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+FILE* fp;
 
 void swap(int i, int j, char* array)
 {
@@ -8,35 +9,54 @@ void swap(int i, int j, char* array)
 	array[j] = temp;
 }
 
-void printPermutations(int size, int origionalSize, char* word)
+void printPermutations(int permutationSize, int origionalSize, char* word)
 {
 
 	int i;
-	if(size == 0)
+
+	if(permutationSize == 0)
 	{
+		//reset the pointer to the initial location
 		char* printer = word-origionalSize; 
+
 		for(i=0;i<origionalSize;i++)
 		{
-			printf("%c ", printer[i]);
+			//print each char with a space in between
+			fprintf(fp,"%c ", printer[i]);
 		}
-		printf("\n");//yes\n");
+
+		fprintf(fp,"\n");
+
 		return;
 	}
+
 	char temp;
-	for(i=0;i<size;i++)
+
+	//for each letter, swap with the first character, 
+	//permute the string after the first character, 
+	//then swap the first character back
+	for(i=0;i<permutationSize;i++)
 	{
 		swap(0,i,word);
-		//printf("%c",word[0]);
-		printPermutations(size-1,origionalSize,word+1);
+
+		printPermutations(permutationSize-1,origionalSize,word+1);
+
+		swap(0,i,word);
 	}
+}
+
+void permuteString(char* string)
+{
+	//initialize with the string size
+	printPermutations(strlen(string), strlen(string), string);
 }
 
 int main(int argc, char** argv)
 {
+	fp = fopen("allTestCases.txt", "w");
+	//0-8 is all the possible moves for the tick tack toe game (when in test mode)
 	char word[]= "876543210";
-	int length = strlen(word);
-	int i=0;
-	printPermutations(length, length,word);
-	printf("%d\n",i);
+	//printing all permutations
+	permuteString(word);
 	return 0;
 }
